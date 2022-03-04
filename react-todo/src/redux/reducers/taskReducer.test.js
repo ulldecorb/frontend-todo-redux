0375c502d566c16421689cf9b353714c1deb243e
ulldecorb <ausias.bertran@gmail.com>
2022-03-04 01:52:10 +0100

    Create tasksReducer type UPDATE_TASK unit test

--- react-todo/src/redux/reducers/taskReducer.test.js
+++ react-todo/src/redux/reducers/taskReducer.test.js
@@ -10,10 +10,26 @@ describe('tasksReducer', () => {
     expect(expected).toEqual(result);
   });
   test('delete task from task state by id', () => {
-    const actionCreator = { type: 'DELETE_TASK', taskId: '1234' };
+    const actionCreator = {
+      type: 'DELETE_TASK',
+      taskId: '1234'
+    };
     const mockTasks = [{ id: '1234' }];
     const expected = tasksReducer(mockTasks, actionCreator);
     const result = [];
+    expect(expected).toEqual(result);
+  });
+  test('delete task from task state by id', () => {
+    const actionCreator = {
+      type: 'UPDATE_TASK',
+      updatedTask: {
+        id: '1234',
+        task: 'updated task'
+      }
+    };
+    const mockTasks = [{ id: '1234', task: 'initial task' }];
+    const expected = tasksReducer(mockTasks, actionCreator);
+    const result = [{ id: '1234', task: 'updated task' }];
     expect(expected).toEqual(result);
   });
 });
