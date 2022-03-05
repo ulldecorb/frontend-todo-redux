1664b50702cd8b92c36351ea24e2e63f86865d93
ulldecorb <ausias.bertran@gmail.com>
2022-03-05 01:12:05 +0100

    Remove reportWebVitals

--- react-todo/src/components/TodoList/todoList.test.js
+++ react-todo/src/components/TodoList/todoList.test.js
@@ -1,7 +1,22 @@
-import '@testing-library/jest-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+import store from '../../redux/store/index';
 
+import TodoList from './index';
+
+// const mockTask = () => [{ id: '1', task: 'complete test', completed: false }];
 describe('When TodoList renders', () => {
-  test('', () => {
-    expect(true).toBe(true);
+  test('handleToggleTodoCheck is invoked', () => {
+    const mockDispatch = jest.fn();
+    const component = render(
+      <Provider store={store}>
+        <TodoList dispatch={mockDispatch} />
+      </Provider>
+    );
+    const inputCheck = component.getByTestId('checkboxInput');
+    fireEvent.click(inputCheck);
+    expect(mockDispatch).toHaveBeenCalledTimes(0);
   });
 });
